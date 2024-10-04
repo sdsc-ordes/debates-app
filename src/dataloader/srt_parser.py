4abbce7bdd90fd19c4ad172dbcd9d5ef3033f9d4
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-04 11:32:50 +0200

    feat: add dataloader package for the data proecessing

---
+++ src/dataloader/srt_parser.py
@@ -0,0 +1,2 @@
+def process_data():
+    print("hello from process data") 
--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import typer
+from dataloader import srt_parser
 
 
 def main(name: str):
     print(f"Hello {name}")
+    srt_parser.process_data()
 
 
 if __name__ == "__main__":
