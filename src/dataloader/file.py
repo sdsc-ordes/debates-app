aebc70afd0b0a1229173e6e68e48beb8a69e177d
sabinem <5292683+sabinem@users.noreply.github.com>
2024-10-22 21:54:53 +0200

    fix: seperate parser from mongo output
    have seperate output functions for parser and mongodb

--- src/dataloader/file.py
+++ src/dataloader/file.py
@@ -13,6 +13,13 @@ def write_output_to_file(processed_data, output):
     print(f"output can be found at {output}")
 
 
+def write_parsed_data_to_file(parsed_data, output):
+    """write parsed video data to a json file"""
+    with open(output, 'w', encoding='utf-8') as file:
+        json.dump(parsed_data, file, indent=4)
+    print(f"output can be found at {output}")
+
+
 def extract_iso_date_from_filename(filepath: str):
     """file name is assumed as HRC_20220328.srt"""
     try:
