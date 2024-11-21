a17da39659d444fbcc71c086fc8452e0147d1536
sabinem <5292683+sabinem@users.noreply.github.com>
2024-11-21 08:03:22 +0100

    chore: remove files_to_mongo cli command as it is not needed

--- src/dataloader/file.py
+++ src/dataloader/file.py
@@ -7,10 +7,3 @@ def write_parsed_data_to_file(parsed_data, output):
     with open(output, 'w', encoding='utf-8') as file:
         json.dump(parsed_data, file, indent=4)
     print(f"output can be found at {output}")
-
-
-def get_data_from_file(filepath):
-    """reads data from a file and returns the data"""
-    with open(filepath, "r") as file:
-        data = file.read()
-    return data
